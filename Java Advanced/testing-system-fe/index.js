76da3630ed0b945f73f0e457eb5e4b4a19124f5f
ba vu <lexuanbavu@gmail.com>
2026-07-13 21:14:49 +0700

    Push buổi 5 - Java Advanced: Chức năng tìm kiếm vào giao diện

--- Java Advanced/testing-system-fe/index.js
+++ Java Advanced/testing-system-fe/index.js
@@ -26,11 +26,20 @@ function changeTheme(themeValue) {
 }
 
 function loadData() {
+    // lấy ra các gtri cần tìm kiếm
+    var usernameSearch = $("#usernameSearch").val();
+    var fullNameSearch = $("#fullNameSearch").val();
+    var emailSearch = $("#emailSearch").val();
+    var departmentIdSearch = $("#deparmentSearchID").val();
+    var positionIdSearch = $("#positionSearchID").val();
+
+    var subUrl = `?username=${usernameSearch}&departmentId=${departmentIdSearch}&fullName=${fullNameSearch}&email=${emailSearch}&positionId=${positionIdSearch}`;
+
     // call api đến mockapi.io đe lấy ds account
     // jqAjax
     $.ajax({
         type: "GET",
-        url: baseUrl,
+        url: baseUrl + subUrl,
         dataType: "JSON",
         success: function (response) {
             // call api thanh cong
@@ -45,6 +54,7 @@ function loadData() {
                     " style='height: 50px' alt='Image' /></td>";
                 tableContent += "<td>" + accounts[i].username + "</td>";
                 tableContent += "<td>" + accounts[i].fullName + "</td>";
+                tableContent += "<td>" + accounts[i].email + "</td>";
                 tableContent += "<td>" + accounts[i].departmentName + "</td>";
                 tableContent += "<td>" + accounts[i].positionName + "</td>";
                 tableContent +=
@@ -244,6 +254,10 @@ function loadDepartment() {
             }
             $("#inputDepartmentName").empty();
             $("#inputDepartmentName").append(content);
+            // load cho ô tìm kiếm
+            $("#deparmentSearchID").empty();
+            $("#deparmentSearchID").append("<option value=''>Tất cả</option>");
+            $("#deparmentSearchID").append(content);
         },
         error: function (error) {
             alert("Call api get department thất bại");
@@ -265,6 +279,10 @@ function loadPosition() {
             }
             $("#inputPositionName").empty();
             $("#inputPositionName").append(content);
+            // load cho ô tìm kiếm
+            $("#positionSearchID").empty();
+            $("#positionSearchID").append("<option value=''>Tất cả</option>");
+            $("#positionSearchID").append(content);
         },
         error: function (error) {
             alert("Call api get position thất bại");
