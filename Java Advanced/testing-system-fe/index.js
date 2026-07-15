a3efc4e3ca33578a324adfa1884625cdf0b5f405
ba vu <lexuanbavu@gmail.com>
2026-07-15 21:38:53 +0700

    Push nội dung buổi 6 - Java Advanced: Hoàn thiện tính năng phân trang

--- Java Advanced/testing-system-fe/index.js
+++ Java Advanced/testing-system-fe/index.js
@@ -1,4 +1,5 @@
 var accounts = []; // mảng chứa account
+var pageNumber = 0; // quản lý trang hiện tại (mặc định là trang đầu tiên - chỉ số 0)
 var v_idUpdate = -1;
 var vTheme = "";
 var baseUrl = "http://localhost:8080/api/v1/accounts";
@@ -33,7 +34,11 @@ function loadData() {
     var departmentIdSearch = $("#deparmentSearchID").val();
     var positionIdSearch = $("#positionSearchID").val();
 
-    var subUrl = `?username=${usernameSearch}&departmentId=${departmentIdSearch}&fullName=${fullNameSearch}&email=${emailSearch}&positionId=${positionIdSearch}`;
+    //lấy ra các giá trị liên quan đến phân trang
+    var size = $("#numberOfRecordId").val();
+
+    var subUrl = `?username=${usernameSearch}&departmentId=${departmentIdSearch}&fullName=${fullNameSearch}&email=${emailSearch}&positionId=${positionIdSearch}&size=${size}&page=${pageNumber}`;
+    console.log(baseUrl + subUrl);
 
     // call api đến mockapi.io đe lấy ds account
     // jqAjax
@@ -43,7 +48,7 @@ function loadData() {
         dataType: "JSON",
         success: function (response) {
             // call api thanh cong
-            accounts = response;
+            accounts = response.content;
             var tableContent = "";
             for (let i = 0; i < accounts.length; i++) {
                 tableContent += "<tr>";
@@ -66,18 +71,43 @@ function loadData() {
                     ")'>Delete</button></td>";
                 tableContent += "</tr>";
             }
-            //         {
-            //     "id": 1,
-            //     "username": "annguyen1",
-            //     "fullName": "Nguyễn Văn An",
-            //     "departmentName": "Marketing",
-            //     "positionName": "DEV"
-            // },
-            // trước khi show data thì clear bảng trước
-            //jqEmpty
             $("#tableBoby").empty();
             // jqAppend
             $("#tableBoby").append(tableContent);
+
+            // hiển thị thông tin paging  pagingId
+            $("#pagingId").empty();
+
+            // Nút TRƯỚC (<<)
+            var prevPageClick = response.first
+                ? ""
+                : `onclick="changePage(${pageNumber - 1})"`;
+            $("#pagingId").append(
+                `<li ${response.first == true ? `class="disabled"` : ``}><a href="#" ${prevPageClick}>&laquo;</a></li>`,
+            );
+
+            // load các trang tương ứng ra
+            var totalPage = response.totalPages;
+            for (let i = 0; i < totalPage; i++) {
+                if (i == pageNumber) {
+                    // Thêm class="active" để nút sáng màu xanh chuẩn Bootstrap 3
+                    $("#pagingId").append(
+                        `<li class="active"><a href="#">${i + 1}</a></li>`,
+                    );
+                } else {
+                    $("#pagingId").append(
+                        `<li><a href="#" onclick="changePage(${i})">${i + 1}</a></li>`,
+                    );
+                }
+            }
+
+            // Nút SAU (>>)
+            var nextPageClick = response.last
+                ? ""
+                : `onclick="changePage(${pageNumber + 1})"`;
+            $("#pagingId").append(
+                `<li ${response.last == true ? `class="disabled"` : ``}><a href="#" ${nextPageClick}>&raquo;</a></li>`,
+            );
         },
         error: function (error) {
             alert("Call api get accounts thất bại");
@@ -102,7 +132,7 @@ function onDelete(idDelete) {
     }
 }
 
-function onCreate(idDelete) {
+function onCreate() {
     if (v_idUpdate > 0) {
         alert("Đang update, ko thể tạo mới dc");
         return;
@@ -114,7 +144,6 @@ function onCreate(idDelete) {
     var v_departmentID = $("#inputDepartmentName").val();
     var v_positionID = $("#inputPositionName").val();
 
-    // đưa các dữ liệu trên vào object // object của js
     var account = {
         username: v_username,
         fullName: v_fullName,
@@ -122,19 +151,15 @@ function onCreate(idDelete) {
         departmentId: v_departmentID,
         positionId: v_positionID,
     };
-    //https://images2.thanhnien.vn/528068263637045248/2024/1/25/e093e9cfc9027d6a142358d24d2ee350-65a11ac2af785880-17061562929701875684912.jpg
-    // call api dể thêm mới account
+
     $.ajax({
         type: "POST",
         url: baseUrl,
-        data: JSON.stringify(account), // chuyển account từ obejct của JS thành JSON
+        data: JSON.stringify(account),
         contentType: "application/json",
         success: function (response) {
             alert("Thêm dữ liệu thành công");
-            // hiển thị lại ds account
             loadData();
-            // clear dữ lieu 3 ô username, fullName, age ở tren
-            //jqValSet
             $("#inputAvatar").val("");
             $("#inputUsername").val("");
             $("#inputFullname").val("");
@@ -147,15 +172,7 @@ function onCreate(idDelete) {
     });
 }
 
-// jqSubmit
-// $("#accountForm").submit(function (e) {
-//     e.preventDefault();
-
-// });
-
 $("#submit").click(function (e) {
-    // nếu v_idUpdate <= 0    thì sẽ tạo mới
-    // nếu v_idUpdate > 0 thì sẽ update
     if (v_idUpdate <= 0) {
         onCreate();
     } else {
@@ -174,9 +191,7 @@ function resetForm() {
 }
 
 function onHandleEdit(idUpdate) {
-    // mo modal
     $("#modal-id").modal("show");
-    // call api get by id đẻ lấy lấy dữ liệu ra để hiển thị lên các ô input
     $.ajax({
         type: "GET",
         url: baseUrl + "/" + idUpdate,
@@ -184,15 +199,13 @@ function onHandleEdit(idUpdate) {
         success: function (response) {
             $(".modal-title").empty();
             $(".modal-title").append("<div>Update Account</div>");
-            // hien thi ra cac o input tuong ung
             $("#inputAvatar").val(response.avatar);
             $("#inputUsername").val(response.username);
             $("#inputFullname").val(response.fullName);
             $("#inputEmail").val(response.email);
-            // cho các ô select chọn đúng gtri của phòng ban và chức vụ
             $("#inputDepartmentName").val(response.departmentId);
             $("#inputPositionName").val(response.positionId);
-            v_idUpdate = idUpdate; // lưu lại id cần update
+            v_idUpdate = idUpdate;
         },
         error: function (error) {
             alert("Call api lấy thông tin thất bại");
@@ -200,14 +213,13 @@ function onHandleEdit(idUpdate) {
     });
 }
 
-function onUpdate(idDelete) {
+function onUpdate() {
     var v_avatar = $("#inputAvatar").val();
     var v_username = $("#inputUsername").val();
     var v_fullName = $("#inputFullname").val();
     var v_email = $("#inputEmail").val();
     var v_departmentID = $("#inputDepartmentName").val();
     var v_positionID = $("#inputPositionName").val();
-    // lay ra doi tuong can update
     var accountUpdate = {
         username: v_username,
         fullName: v_fullName,
@@ -215,7 +227,6 @@ function onUpdate(idDelete) {
         departmentId: v_departmentID,
         positionId: v_positionID,
     };
-    // call api update
     $.ajax({
         type: "PUT",
         url: baseUrl + "/" + v_idUpdate,
@@ -223,9 +234,7 @@ function onUpdate(idDelete) {
         contentType: "application/json",
         success: function (response) {
             alert("Update dữ liệu thành công");
-            // hiển thi ls account
             loadData();
-            //jqValSet
             v_idUpdate = -1;
             $("#inputAvatar").val("");
             $("#inputUsername").val("");
@@ -240,21 +249,17 @@ function onUpdate(idDelete) {
 }
 
 function loadDepartment() {
-    //call api den BE lay ds department
     $.ajax({
         type: "GET",
         url: baseUrlDepartment,
         dataType: "JSON",
         success: function (response) {
-            //
             var content = "";
             for (let i = 0; i < response.length; i++) {
-                // content += "<option value=''>" + response[i].name + "</option>";
                 content += `<option value="${response[i].id}">${response[i].name}</option>`;
             }
             $("#inputDepartmentName").empty();
             $("#inputDepartmentName").append(content);
-            // load cho ô tìm kiếm
             $("#deparmentSearchID").empty();
             $("#deparmentSearchID").append("<option value=''>Tất cả</option>");
             $("#deparmentSearchID").append(content);
@@ -271,15 +276,12 @@ function loadPosition() {
         url: baseUrlPosition,
         dataType: "JSON",
         success: function (response) {
-            //
             var content = "";
             for (let i = 0; i < response.length; i++) {
-                // content += "<option value=''>" + response[i].name + "</option>";
                 content += `<option value="${response[i].id}">${response[i].name}</option>`;
             }
             $("#inputPositionName").empty();
             $("#inputPositionName").append(content);
-            // load cho ô tìm kiếm
             $("#positionSearchID").empty();
             $("#positionSearchID").append("<option value=''>Tất cả</option>");
             $("#positionSearchID").append(content);
@@ -289,3 +291,15 @@ function loadPosition() {
         },
     });
 }
+
+// Hàm chuyển trang khi click vào số trang hoặc nút trước/sau
+function changePage(page) {
+    pageNumber = page; // Cập nhật trang hiện tại
+    loadData(); // Gọi lại API để tải dữ liệu mới
+}
+
+// Bắt sự kiện thay đổi số dòng hiển thị
+$("#numberOfRecordId").change(function () {
+    pageNumber = 0; // Reset về trang đầu tiên
+    loadData();
+});
