652023a0d0917e5a45763a5f26614086f30a6f4d
ba vu <lexuanbavu@gmail.com>
2026-08-10 21:49:32 +0700

    Bổ sung thêm constructor cho Person.ts

--- ReactJS/TypeScript/Lab2/Person.ts
+++ ReactJS/TypeScript/Lab2/Person.ts
@@ -1,7 +1,7 @@
 class Person {
-    id!: number;
-    name!: string;
-    address!: string;
+    id: number;
+    name: string;
+    address: string;
 
     go(): void {
         console.log("I can go!!");
@@ -13,6 +13,12 @@ class Person {
         console.log(
             `id: ${this.id}, name: ${this.name}, address: ${this.address}`,
         );
+    }
+
+    constructor(id: number = 0, name: string = "", address: string = "") {
+        this.id = id;
+        this.name = name;
+        this.address = address;
     }
 }
 
