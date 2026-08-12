532d1299b23f125f47aac11ad34e9029ac9db218
ba vu <lexuanbavu@gmail.com>
2026-08-12 21:08:49 +0700

    Nội dung spread operator: toán tử trải

--- ReactJS/TypeScript/Lab5/Demo.js
+++ ReactJS/TypeScript/Lab5/Demo.js
@@ -1,37 +1,18 @@
+"use strict";
 //Destructuring: phá vỡ cấu trúc
-
 let account = {
     id: 1,
     name: "BaVu",
     balance: 1000
 };
-
 // hãy tách giá trị của các biến account ra thành các biến riêng biệt
 // let v_id = account.id;
 // let v_name = account.name;
 // let v_balance = account.balance;
-
 //Destructuring:
-let {id: v_id, name: v_name, balance: v_balance} = account;
+let { id: v_id, name: v_name, balance: v_balance } = account;
 console.log(`Id: ${v_id}, Name: ${v_name}, Balance: ${v_balance}`);
-
 // Mảng
 let name_railway = ["Ga Da Nang", "Ga Hue", "Ga Ho Chi Minh"];
 let [v_ga1, v_ga2, v_ga3] = name_railway;
 console.log(`Ga 1: ${v_ga1}, Ga 2: ${v_ga2}, Ga 3: ${v_ga3}`);
-
-// ... spread operator: toán tử trải
-// let mentors = {
-//     id: 1,
-//     name: "BaVu",
-//     balance: 1000,
-//     address: "DN",
-//     age: 30
-// };
-
-let mentors = {
-    ...account,
-    address: "DN",
-    age: 23
-};
-console.log(mentors);
