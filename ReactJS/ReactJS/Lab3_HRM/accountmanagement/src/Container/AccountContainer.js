698f3c592c0d54682456bd72067cceed776d3bcc
ba vu <lexuanbavu@gmail.com>
2026-08-24 21:08:43 +0700

    feat: Lab20.3_Xây dựng giao diện thêm mới Acccount

--- ReactJS/ReactJS/Lab3_HRM/accountmanagement/src/Container/AccountContainer.js
+++ ReactJS/ReactJS/Lab3_HRM/accountmanagement/src/Container/AccountContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container } from "reactstrap";
 import CreateButton from "../Components/Account/CreateButton";
+import ModalCreateNewAccount from "../Components/Account/CreateNewAccount/ModalCreateNewAccount";
 import ResultForm from "../Components/Account/ResultForm";
 
 
@@ -9,6 +10,8 @@ function AccountContainer(props) {
     <Container>
       {/* Nút thêm mới */}
       <CreateButton />
+      {/* Form thêm mới Account*/}
+      <ModalCreateNewAccount />
       {/* Form kết quả */}
       <ResultForm></ResultForm>
     </Container>
