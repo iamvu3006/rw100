a883cde8f945a7250a0484c59f1fb8964624b7e7
ba vu <lexuanbavu@gmail.com>
2026-09-04 21:11:42 +0700

    Lab20.12_CALL API_Template_Tạo mới Account.

--- ReactJS/ReactJS/Lab3_HRM/accountmanagement/src/Container/AccountContainer.js
+++ ReactJS/ReactJS/Lab3_HRM/accountmanagement/src/Container/AccountContainer.js
@@ -5,7 +5,7 @@ import CreateButton from "../Components/Account/CreateButton";
 import ModalCreateNewAccount from "../Components/Account/CreateNewAccount/ModalCreateNewAccount";
 import ResultForm from "../Components/Account/ResultForm";
 // import Axios from "axios"; // Import thư viện Axios để sử dụng
-import { getListAccountAPI } from "../api/AccountApi";
+import { getListAccountAPI, addAccountNewAPI } from "../api/AccountApi";
 import { getListDepartmentAPI } from "../api/DepartmentApi";
 import { getListPositionAPI } from "../api/PositionApi";
 
@@ -29,45 +29,41 @@ function AccountContainer(props) {
   };
   // Hàm Callback xử lý khi nhấn nút Create ở InputForm
   let onHandleCreateNewAccount = (accountNew) => {
-    let updatedList = [...listAccount, accountNew];
-    setListAccount(updatedList);
-    localStorage.setItem("listAccount", JSON.stringify(updatedList));
+    let accountNew_API = {
+      email: accountNew.email,
+      username: accountNew.username,
+      fullname: accountNew.fullname,
+      departmentId: accountNew.department,
+      positionId: accountNew.position,
+    };
+    // Gọi hàm call API
+    addAccountNewAPI(accountNew_API).then((response) => {
+      // Sau khi tạo dữ liệu thành công gọi hàm để load lại listAccount
+      fetchListAccount();
+    });
     // Thực hiện đóng Form sau khi thêm mới
     setShowForm(false);
   };
 
   // Hàm load dữ liệu API cho Account
   let fetchListAccount = function () {
-    const baseURL = `http://localhost:8080`; // Địa chỉ Server
-
-    Axios.get(`${baseURL}/api/v1/accounts`)
-      .then((response) => {
-        let listAccounts_API = response.data; // ListAccount nhận được khi Call API
-        setListAccount(listAccounts_API); // Set lại State
-      })
-      .catch((error) => console.log("Lỗi fetchListAccount:", error));
+    getListAccountAPI().then((response) => {
+      setListAccount(response);
+    });
   };
 
   // Hàm load dữ liệu API cho Department
   let fetchListDepartment = function () {
-    const baseURL = `http://localhost:8080`; // Link địa chỉ Server
-    Axios.get(`${baseURL}/api/v1/departments`)
-      .then((response) => {
-        let listDepartment_API = response.data; // ListDepartment nhận được khi Call API
-        setListDepartment(listDepartment_API); // Set lại State
-      })
-      .catch((error) => console.log("Lỗi fetchListDepartment:", error));
+    getListDepartmentAPI().then((response) => {
+      setListDepartment(response);
+    });
   };
 
   // Hàm load dữ liệu API cho Position
   let fetchListPosition = function () {
-    const baseURL = `http://localhost:8080`; // Link địa chỉ Server
-    Axios.get(`${baseURL}/api/v1/possitions`)
-      .then((response) => {
-        let listPosition_API = response.data; // ListPosition nhận được khi Call API
-        setListPosition(listPosition_API); // Set lại State
-      })
-      .catch((error) => console.log("Lỗi fetchListPosition:", error));
+    getListPositionAPI().then((response) => {
+      setListPosition(response);
+    });
   };
 
   // Khai báo useEffect khi component được mount
