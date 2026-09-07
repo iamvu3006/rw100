fd0a3f952ddd1dd1f330faf687ff5a84e6647357
ba vu <lexuanbavu@gmail.com>
2026-09-07 20:57:09 +0700

    feat: Lab11.4_Xây dựng SignUp Login.

--- ReactJS/ReactJS/Lab4/demo_router/src/Components/Login.js
+++ ReactJS/ReactJS/Lab4/demo_router/src/Components/Login.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login(props) {
+  // Khai báo các state để lưu dữ liệu cho các ô nhập liệu
+  let [Email, SetEmail] = useState("");
+  let [Password, SetPassword] = useState("");
+
+  // Hàm xử lý khi nhấn nút Login
+  let handleLogin = () => {
+    alert("Email: " + Email);
+    alert("Password: " + Password);
+  };
+
   return (
-    <div>
-      <h2>This is Login Page</h2>
+    <div className="container" style={{ width: "70%" }}>
+      <div className="row">
+        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <br />
+          <br />
+          <br />
+          <h3>LOGIN</h3>
+          <br />
+          <div className="form-group">
+            <label htmlFor="Email">Email:</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              value={Email}
+              onChange={(event) => SetEmail(event.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Password">Password:</label>
+            <input
+              type="Password"
+              className="form-control"
+              placeholder="Enter Password"
+              value={Password}
+              onChange={(event) => SetPassword(event.target.value)}
+            />
+          </div>
+          <button type="button" className="btn btn-info" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
