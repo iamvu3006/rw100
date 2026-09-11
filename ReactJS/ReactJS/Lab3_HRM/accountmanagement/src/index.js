dcf55bb61ba49c3e539215b96abb7cb8d4386ceb
ba vu <lexuanbavu@gmail.com>
2026-09-11 21:14:16 +0700

    feat: Lab20.13_Redux_Tạo cấu trúc chương trình.

--- ReactJS/ReactJS/Lab3_HRM/accountmanagement/src/index.js
+++ ReactJS/ReactJS/Lab3_HRM/accountmanagement/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
+import storeRedux from "./Redux/Store/storeRedux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={storeRedux}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
