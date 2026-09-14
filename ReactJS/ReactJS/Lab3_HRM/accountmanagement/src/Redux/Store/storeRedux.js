437080e36558c47e2ae969c053ffc2d88071384c
ba vu <lexuanbavu@gmail.com>
2026-09-14 19:55:08 +0700

    feat: Lab20.15_Redux_Cài đặt Redux Dev Tool.

--- ReactJS/ReactJS/Lab3_HRM/accountmanagement/src/Redux/Store/storeRedux.js
+++ ReactJS/ReactJS/Lab3_HRM/accountmanagement/src/Redux/Store/storeRedux.js
@@ -1,6 +1,10 @@
 import { createStore } from "redux";
 import RootReducers from "../Reducer/RootReducer";
 
-const storeRedux = createStore(RootReducers);
+const storeRedux = createStore(
+  RootReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
+);
 
 export default storeRedux;
